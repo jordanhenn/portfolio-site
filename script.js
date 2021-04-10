5aff435564ccbdc0c05574df117078a4e97c43b9
Jordan Henn <jordanwalkerhenn@gmail.com>
2021-04-10 12:54:38 -0700

    added portfolio item

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $('.js-bio').on("click",function(event){
         <p>Loves <span class="important">making users’ lives easier</span> than they could have ever imagined.</p>
         <p><span class="important">Addicted to learning</span>--often caught messing around with new APIs.</p> 
         <p>Interested in joining a <span class="important">team of programmers.</span></p>
-        <p>Fan of 3D modelling/animation, theme parks, and the XFL.</p>
+        <p>Fan of 3D modelling/animation, sailing, and theme parks.</p>
     </div>`);
     $('.links').toggleClass("hidden");
 });
@@ -27,14 +27,27 @@ $('.js-port').on("click",function(event){
     <div class="portfolio-items">
     <div class="port-item-wrap">
         <div class="imagebox">
-            <img src="./cantdecide.png" alt="CantDecide"/>
+            <img src="./helsing.png" alt="HG Job Tracker"/>
         </div>
         <div class="app-description">
-            <h3>CantDecide</h3>
-            <p class="tech">HTML, CSS, JS</p>
-            <p class="app-descript">An app that provides a random movie suggestion and available streaming locations using the TMDb and Utelly APIs. Meant for people who have trouble choosing movies to watch. Built in order to better understand API documentation.</p>
+            <h3>HG Job Tracker</h3>
+            <p class="tech">HTML, CSS, JS, React, Express</p>
+            <p class="app-descript">Full-stack app developed for HOA Management company. Used to track billings and progress on reserve study projects. Also helps automate aspects of the manager/employee/client relationship cycle. Developed after it was discovered the previous system was leading to tens of thousands of dollars of missing billings.</p>
             <div class="live-links-wrap">
-                <p><a target="_blank" href="https://github.com/jordanhenn/cantdecide">GitHub</a> | <a target="_blank" href="https://jordanhenn.github.io/cantdecide/">Live</a></p>
+                <p><a target="_blank" href="https://github.com/jordanhenn/helsing-client">GitHub</a> | <a target="_blank" href="https://helsing-client.vercel.app/">Live</a></p>
+            </div>
+        </div>
+    </div>
+    <div class="port-item-wrap">
+        <div class="imagebox">
+            <img src="./fastfoodevolved.png" alt="Fast Food Evolved"/>
+        </div>
+        <div class="app-description">
+            <h3>Fast Food Evolved</h3>
+            <p class="tech">HTML, CSS, JS, React, Express</p>
+            <p class="app-descript">Full-stack app where users can select different items (buns, sauces, fillings) from different fast food establishments and combine them into a new creation. Meant for fast food lovers looking to create the ultimate hybrid. Built to better understand React, Express, and svg image manipulation. Used Adobe Illustrator to create graphics.</p>
+            <div class="live-links-wrap">
+                <p><a target="_blank" href="https://github.com/jordanhenn/fastfood-client">GitHub</a> | <a target="_blank" href="https://fastfood-client.jordanhenn.vercel.app/">Live</a></p>
             </div>
         </div>
     </div>
@@ -53,14 +66,14 @@ $('.js-port').on("click",function(event){
     </div>
     <div class="port-item-wrap">
         <div class="imagebox">
-            <img src="./fastfoodevolved.png" alt="Fast Food Evolved"/>
+            <img src="./cantdecide.png" alt="CantDecide"/>
         </div>
         <div class="app-description">
-            <h3>Fast Food Evolved</h3>
-            <p class="tech">HTML, CSS, JS, React, Express</p>
-            <p class="app-descript">Full-stack app where users can select different items (buns, sauces, fillings) from different fast food establishments and combine them into a new creation. Meant for fast food lovers looking to create the ultimate hybrid. Built to better understand React, Express, and svg image manipulation. Used Adobe Illustrator to create graphics.</p>
+            <h3>CantDecide</h3>
+            <p class="tech">HTML, CSS, JS</p>
+            <p class="app-descript">An app that provides a random movie suggestion and available streaming locations using the TMDb and Utelly APIs. Meant for people who have trouble choosing movies to watch. Built in order to better understand API documentation.</p>
             <div class="live-links-wrap">
-                <p><a target="_blank" href="https://github.com/jordanhenn/fastfood-client">GitHub</a> | <a target="_blank" href="https://fastfood-client.jordanhenn.vercel.app/">Live</a></p>
+                <p><a target="_blank" href="https://github.com/jordanhenn/cantdecide">GitHub</a> | <a target="_blank" href="https://jordanhenn.github.io/cantdecide/">Live</a></p>
             </div>
         </div>
     </div>
